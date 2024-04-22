c3d3e2f80b64abce68eaa6d6bc9ffdda7ee55621
Thalyson Luiz <thalysonluiz@gmail.com>
2024-04-22 13:11:52 -0300

    salvar evento no banco

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import fastify from "fastify";
 import z from "zod";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
 
-app.post('/events', (request, reply) => {
+const prisma = new PrismaClient({
+  log: ['query'],
+});
+
+app.post('/events', async (request, reply) => {
   const createEventSchema = z.object({
     title: z.string().min(4),
     details: z.string().nullable(),
@@ -12,7 +17,16 @@ app.post('/events', (request, reply) => {
 
   const data = createEventSchema.parse(request.body)
 
-  return data
+  const event = await prisma.event.create({
+    data: {
+      title: data.title,
+      details: data.details,
+      maximumAttendees: data.maximumAttendees,
+      slug: 'nlw-unite'
+    }
+  })
+
+  return reply.status(201).send({eventId: event.id})
 })
 
 app.listen({
