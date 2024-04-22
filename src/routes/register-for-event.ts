791105c8fe73be9d5ffd9fbaeb9c43fef430e674
Thalyson Luiz <thalysonluiz@gmail.com>
2024-04-22 16:54:48 -0300

    cadastro unique em evento, não ultrapassar limite vagas

--- src/routes/register-for-event.ts
+++ src/routes/register-for-event.ts
@@ -24,6 +24,40 @@ export async function registerForEvent(app: FastifyInstance) {
   }, async (request, reply) => {
     const {eventId} = request.params;
     const {name, email} = request.body;
+
+    const attendeeFromEmail = await prisma.attendee.findUnique({
+      where: {
+        email_eventId: {
+          email,
+          eventId,
+        }
+      },
+    });
+
+    if(attendeeFromEmail !== null) {
+      throw new Error('This e-mail is already registered for this event.')
+    }
+
+    const [event, amountOfAttendeesForEvent] = await Promise.all([
+      prisma.event.findUnique({
+        where: {
+          id: eventId,
+        },
+      }),
+      
+      prisma.attendee.count({
+        where: {
+          eventId,
+        },
+      }),
+
+    ])
+
+
+    if(event?.maximumAttendees && amountOfAttendeesForEvent >= event.maximumAttendees){
+      throw new Error('This event is full.')
+    }
+
     const attendee = await prisma.attendee.create({
       data: {
         name,
@@ -31,6 +65,7 @@ export async function registerForEvent(app: FastifyInstance) {
         eventId,
       },
     })
+
     reply.status(201).send({attendeeId: attendee.id});
   })
 }
