3531739d2597d2f33c3b77a0cbe0b0d39a09a08c
Thalyson Luiz Gomes de Souza <thalyson.souza@inatena.com>
2024-04-22 12:47:35 -0300

    migration

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,12 @@ import fastify from "fastify";
 
 const app = fastify();
 
+app.post('/events', (request, reply) => {
+  const body = request.body
+
+  return body
+})
+
 app.listen({
   port: 3333
 }).then(() => {
